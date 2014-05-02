873c3b23b65f0177d7ea6b261d9e9673527282ac
unknown-user <unknown-user@10.211.55.2>
2014-05-02 20:06:03 +0530

    Initial check-in of module dsgdfgdf

---
+++ MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class A
+{
+	private enum E1 // Doesn't work
+	{
+	}
+
+	protected enum E2 // Doesn't work
+	{
+	}
+
+	internal delegate void D1 ();
+	// Doesn't work
+
+	private A () // Works
+	{
+	}
+
+	private int a;
+	// Works
+
+	internal int Prop { private get; set; }
+	// Works
+
+	private event D1 e;
+	// Works
+
+	protected interface I // Doesn't work
+	{
+	}
+}
